0d6343c8449333f415f967d611a350c444fd666f
Vioteo <prostosokol@gmail.com>
2020-10-17 10:08:51 +0300

    Login screen and chat

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,90 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:digital_hack/map.dart';
-import 'package:digital_hack/profile.dart';
+import 'package:digital_hack/main_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:digital_hack/tasks.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MaterialApp(
+      home: new MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
+  String _email;
+  String _password;
+  final _sizeTextBlack = const TextStyle(fontSize: 20.0, color: Colors.black);
+  final _sizeTextWhite = const TextStyle(fontSize: 20.0, color: Colors.white);
+  final formKey = new GlobalKey<FormState>();
+  BuildContext _context;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Мое приложение',
-      home: BottomNavBar(),
-    );
-  }
-}
-
-class BottomNavBar extends StatefulWidget {
-  @override
-  _BottomNavBarState createState() => _BottomNavBarState();
-}
-
-final Color navIconColor = Colors.white;
-
-class _BottomNavBarState extends State<BottomNavBar> {
-  int _page = 0;
-  GlobalKey _bottomNavigationKey = GlobalKey();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: CurvedNavigationBar(
-        key: _bottomNavigationKey,
-        index: 0,
-        height: 50.0,
-        items: <Widget>[
-          Icon(Icons.explore, size: 30, color: navIconColor),
-          Icon(Icons.line_weight, size: 30, color: navIconColor),
-          Icon(Icons.message, size: 30, color: navIconColor),
-          Icon(Icons.history, size: 30, color: navIconColor),
-          Icon(Icons.perm_identity, size: 30, color: navIconColor),
-        ],
-        backgroundColor: Colors.white,
-        color: Colors.green,
-        animationCurve: Curves.easeInOut,
-        animationDuration: Duration(milliseconds: 300),
-        onTap: (index) {
-          setState(() {
-            _controller.animateToPage(index,
-                duration: Duration(milliseconds: 300), curve: Curves.easeOut);
-          });
-        },
-      ),
-      body: Container(
-        child: PageView(
-          controller: _controller, // assign it to PageView
-          children: <Widget>[
-            MapVkld(),
-            FruitList(),
-            Center(
-              child: Text('Вкладка 3'),
-            ),
-            Center(
-              child: Text('Вкладка 4'),
-            ),
-            Profile() //пример вкладки
-          ],
+    _context = context;
+    return new MaterialApp(
+      home: new Scaffold(
+        body: new Center(
+          child: new Form(
+              key: formKey,
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Container(
+                    child: new TextFormField(
+                      decoration: new InputDecoration(labelText: "Почта"),
+                      keyboardType: TextInputType.emailAddress,
+                      maxLines: 1,
+                      style: _sizeTextBlack,
+                      onSaved: (val) => _email = val,
+                      validator: (val) => !val.contains("@")
+                          ? 'Не действительная почта.'
+                          : null,
+                    ),
+                    width: 400.0,
+                  ),
+                  new Container(
+                    child: new TextFormField(
+                      decoration: new InputDecoration(labelText: "Пароль"),
+                      obscureText: true,
+                      maxLines: 1,
+                      validator: (val) =>
+                          val.length < 6 ? 'Короткий пароль.' : null,
+                      onSaved: (val) => _password = val,
+                      style: _sizeTextBlack,
+                    ),
+                    width: 400.0,
+                    padding: new EdgeInsets.only(top: 10.0),
+                  ),
+                  new Padding(
+                    padding: new EdgeInsets.only(top: 25.0),
+                    child: new MaterialButton(
+                      onPressed: submit,
+                      color: Colors.green,
+                      height: 50.0,
+                      minWidth: 150.0,
+                      child: new Text(
+                        "Войти",
+                        style: _sizeTextWhite,
+                      ),
+                    ),
+                  )
+                ],
+              )),
         ),
       ),
     );
   }
-}
 
-final _controller = PageController();
+  void submit() {
+    final form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      performLogin();
+    }
+  }
+
+  void performLogin() {
+    hideKeyboard();
+    Navigator.push(_context,
+        new MaterialPageRoute(builder: (context) => new BottomNavBar()));
+  }
+
+  void hideKeyboard() {
+    SystemChannels.textInput.invokeMethod('TextInput.hide');
+  }
+}
---
+++ lib/main_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:digital_hack/map.dart';
+import 'package:digital_hack/profile.dart';
+import 'package:digital_hack/tasks.dart';
+import 'package:digital_hack/chat/screens/home_screen.dart';
+
+class BottomNavBar extends StatefulWidget {
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+final Color navIconColor = Colors.white;
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _page = 0;
+  GlobalKey _bottomNavigationKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        key: _bottomNavigationKey,
+        index: 0,
+        height: 50.0,
+        items: <Widget>[
+          Icon(Icons.explore, size: 30, color: navIconColor),
+          Icon(Icons.line_weight, size: 30, color: navIconColor),
+          Icon(Icons.message, size: 30, color: navIconColor),
+          Icon(Icons.history, size: 30, color: navIconColor),
+          Icon(Icons.perm_identity, size: 30, color: navIconColor),
+        ],
+        backgroundColor: Colors.white,
+        color: Colors.green,
+        animationCurve: Curves.easeInOut,
+        animationDuration: Duration(milliseconds: 300),
+        onTap: (index) {
+          setState(() {
+            _controller.animateToPage(index,
+                duration: Duration(milliseconds: 300), curve: Curves.easeOut);
+          });
+        },
+      ),
+      body: Container(
+        child: PageView(
+          controller: _controller, // assign it to PageView
+          children: <Widget>[
+            MapVkld(),
+            FruitList(),
+            MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Chat',
+              theme: ThemeData(
+                primaryColor: Colors.green,
+                accentColor: Color(0xFFFEF9EB),
+              ),
+              home: HomeScreen(),
+            ),
+            Center(
+              child: Text('Вкладка 4'),
+            ),
+            Profile() //пример вкладки
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+final _controller = PageController();
